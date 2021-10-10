eb224e6846920e6a258d276f4eef8e15bc8ae15a
a.yatskevich <a.yatskevich@tinkoff.ru>
2021-10-10 18:11:13 +0500

    homework 4

--- quiz/src/main/java/ru/otus/quiz/service/QuizService.java
+++ quiz/src/main/java/ru/otus/quiz/service/QuizService.java
@@ -1,24 +1,22 @@
 package ru.otus.quiz.service;
 
 import org.springframework.stereotype.Service;
+import ru.otus.quiz.domain.Student;
 import ru.otus.quiz.service.impl.CountCorrectAnswersGradeService;
 import ru.otus.quiz.service.impl.QuestionAskingService;
 
 @Service
 public class QuizService {
 
-    private final WelcomeService welcomeService;
     private final QuestionAskingService askingService;
     private final GradeService gradeService;
 
     public QuizService(WelcomeService welcomeService, QuestionAskingService askingService, CountCorrectAnswersGradeService gradeService) {
-        this.welcomeService = welcomeService;
         this.askingService = askingService;
         this.gradeService = gradeService;
     }
 
-    public void runQuiz() {
-        var student = welcomeService.welcome();
+    public void runQuiz(Student student) {
         var countCorrectAnswers = askingService.runAsk();
         gradeService.runGrade(student, countCorrectAnswers);
     }
--- quiz/src/test/java/ru/otus/quiz/mapper/QuestionMapperTest.java
+++ quiz/src/test/java/ru/otus/quiz/mapper/QuestionMapperTest.java
@@ -1,18 +1,24 @@
 package ru.otus.quiz.mapper;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import ru.otus.quiz.domain.Question;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
+@ContextConfiguration(classes = QuestionMapper.class)
 class QuestionMapperTest {
 
     private static final String QUESTION_STRING_LINE = "History. What year was serfdom abolished in Russian Empire?;3;1853;1857;1861;1867;";
     private static final int QUESTION_SERIAL_NUMBER = 1;
 
-    private final QuestionMapper questionMapper = new QuestionMapper();
+    @Autowired
+    private QuestionMapper questionMapper;
 
     @Test
     void mapStringLineToQuestionTest() {
